a7a05ad64d485c329316899d4c17d6b9e17336d2
Zachary Truong <zacharytruong@gmail.com>
2021-09-11 11:21:51 -0500

    Clean codes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rock-paper-scissors
-Simple Rock Paper Scissors game
+Play a rock-paper-scisscors game until a player or computer win 3 rounds.
+In this demo, I can demonstrate the following skills:
+1. Functions, nested functions
+2. If... else, and switch conditions
+3. While loops
+4. Keep scores and score calculation
+5. Clean codes
--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,58 @@
-// Declare scores
+// Declare global variables
 let playerScore = 0;
 let compScore = 0;
-let totalRounds = 5;
-// Play a single round
-const playRound = () => {
-    // Ask for a user's input
-    const playerSelection = prompt('Please choose 1 of 3 options: rock, paper, or scissors:');
-    
-    let playerPlay = playerSelection.toUpperCase();
-    // Create a function to randomly return a number 1 - 3
-    let random = Math.ceil(Math.random() * 3);
-    // Convert the random number to 1 of 3 options in the game
-    const convert = () => {
-        switch (random){
-            case 1:
-                return 'ROCK';
-                break;
-            case 2:
-                return 'PAPER';
-                break;
-            case 3:
-                return 'SCISSORS';
-                break;
-        }
+let totalRounds = 3;
+
+// Generate a random number, then assign it as computer selection
+let randomNumber = Math.ceil(Math.random() * 3);
+function computerPlay(){
+    switch (randomNumber){
+        case 1:
+            return 'ROCK';
+            break;
+        case 2:
+            return 'PAPER';
+            break;
+        case 3:
+            return 'SCISSORS';
+            break;
     }
-    // Set the selection of the computer from the random number
-    const computerPlay = convert();
+}
 
-    // Print out player and computer selections
-    console.log(`Player's selection: ${playerPlay}`);
-    console.log(`Computer's selection: ${computerPlay}`);
-
-    // Condition
-    if (playerPlay === computerPlay){
+// Compare user's selection and computer's selection
+function compareSelections(playerPlay, computerPlay){
+    if (playerPlay === computerPlay()){
+        console.log(`Player's selection: ${playerPlay}`);
+        console.log(`Computer's selection: ${computerPlay()}`);
         console.log('Draw');
-    } else if ( (playerPlay === 'ROCK' && computerPlay === 'PAPER') || 
-                (playerPlay === 'SCISSORS' && computerPlay === 'ROCK') || 
-                (playerPlay === 'PAPER' && computerPlay === 'SCISSORS') ){
+    } else if ( (playerPlay === 'ROCK' && computerPlay() === 'PAPER') || 
+                (playerPlay === 'SCISSORS' && computerPlay() === 'ROCK') || 
+                (playerPlay === 'PAPER' && computerPlay() === 'SCISSORS') ){
+        console.log(`Player's selection: ${playerPlay}`);
+        console.log(`Computer's selection: ${computerPlay()}`);
         console.log('You lose');
         compScore = ++compScore;
-        
-    } else if ( (playerPlay === 'ROCK' && computerPlay === 'SCISSORS') || 
-                (playerPlay === 'SCISSORS' && computerPlay === 'PAPER') || 
-                (playerPlay === 'PAPER' && computerPlay === 'ROCK') ){
+    } else if ( (playerPlay === 'ROCK' && computerPlay() === 'SCISSORS') || 
+                (playerPlay === 'SCISSORS' && computerPlay() === 'PAPER') || 
+                (playerPlay === 'PAPER' && computerPlay() === 'ROCK') ){
+        console.log(`Player's selection: ${playerPlay}`);
+        console.log(`Computer's selection: ${computerPlay()}`);
         console.log('You won');
         playerScore = ++playerScore;
-        
     } else {
         console.log('You need to choose 1 of 3 options:rock, paper, or scissors.');
     }
 }
-// Play five games then print the results
-const game = () => {
-    while (compScore < totalRounds && playerScore < totalRounds){
-        playRound();
-        console.log(`Computer's score: ${compScore}`);
-        console.log(`Player's score: ${playerScore}`);
-    }
-    if (compScore < totalRounds || playerScore < totalRounds){
-        declareWinner();
-    }
+
+// Play a single round
+function playRound(){
+    let playerSelection = prompt('Please choose 1 of 3 options: rock, paper, or scissors:');
+    let playerPlay = playerSelection.toUpperCase();
+        compareSelections(playerPlay, computerPlay);
+        
 }
+
+// Declare a winner when player or computer has won total rounds
 function declareWinner(){
     if (compScore > playerScore){
         console.log(`Computer has won ${totalRounds} rounds.`)
@@ -68,4 +60,18 @@ function declareWinner(){
         console.log(`You have won ${totalRounds} rounds.`)
     }
 }
-game();
+
+// Play game until player or computer has won total rounds
+function startGame(){
+    let i = 1;
+    while (compScore < totalRounds && playerScore < totalRounds){
+        console.log(`Game #${i}:`);
+        i += 1;
+        playRound();
+        console.log(`Computer's score: ${compScore}`);
+        console.log(`Player's score: ${playerScore}`);
+    }
+    declareWinner(); 
+}
+
+startGame();
